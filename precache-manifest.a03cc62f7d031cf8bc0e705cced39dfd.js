61c764e9007799bed691e3f61285d257ad97d41a
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-01-12 13:41:59 +0100

    Updates

---
+++ precache-manifest.a03cc62f7d031cf8bc0e705cced39dfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "686fce20002c6d04eb34195fcc9adca4",
+    "url": "/fend-project-map-app-react/static/media/hamburger_menu.686fce20.svg"
+  },
+  {
+    "revision": "ec06fab6da707c2f1a8ff04219817f9e",
+    "url": "/fend-project-map-app-react/static/media/back_icon.ec06fab6.svg"
+  },
+  {
+    "revision": "d9a7d6d4d46880a6b1821c16f1fdc0d3",
+    "url": "/fend-project-map-app-react/static/media/Flag_of_Madeira.d9a7d6d4.svg"
+  },
+  {
+    "revision": "c9bc802c64a38185159d",
+    "url": "/fend-project-map-app-react/static/js/runtime~main.c9bc802c.js"
+  },
+  {
+    "revision": "1616bd9c1c60b02b518f",
+    "url": "/fend-project-map-app-react/static/js/main.1616bd9c.chunk.js"
+  },
+  {
+    "revision": "391fb3c8f45498672d6f",
+    "url": "/fend-project-map-app-react/static/js/1.391fb3c8.chunk.js"
+  },
+  {
+    "revision": "1616bd9c1c60b02b518f",
+    "url": "/fend-project-map-app-react/static/css/main.9c68bab8.chunk.css"
+  },
+  {
+    "revision": "5aef250f63205b8841fcb6ef15d55d3d",
+    "url": "/fend-project-map-app-react/index.html"
+  }
+];
